d28028779669e02af276298b226a3358580f2b63
raosp123 <raosp@tcd.ie>
2021-03-21 18:51:51 +0000

    separation of secGroups and instances

--- src/serversidesrc/core/apisrc/describe_ec2_instances.py
+++ src/serversidesrc/core/apisrc/describe_ec2_instances.py
@@ -6,13 +6,11 @@ from tokened_client import tokened_client_ec2 as ec2_c
 #returns jsons of instances and security groups
 def ec2_api_details():
     ec2 = boto3.client('ec2')
-    response_instances = ec2.describe_instances()
-    reponse_sg = ec2.describe_security_groups()
 
-    securityGroups = json.dumps(response_sg, indent=4)
+    response_instances = ec2.describe_instances()
     instances = json.dumps(response_instances, default = dateserializer, indent=4)
 
-    return securityGroups, instances
+    return instances
     
 
 #Used to convert datetime to string for json
