6ecfe31ab1c1b253e139413a381dc569e40bd2e2
Seva <syrtsovv@tcd.ie>
2021-03-26 17:34:57 +0000

    fixed databases to frontend

--- src/serversidesrc/core/apisrc/databases_to_frontend.py
+++ src/serversidesrc/core/apisrc/databases_to_frontend.py
@@ -1,100 +1,99 @@
 import json
 
-def databases_to_frontend_format(database_data, instances_data):
+def databases_to_frontend_format(security_groups_data, database_data):
 
-    data_list = database_data["Databases"]
+    instances = database_data["DBInstances"]
+    sg_data_list = security_groups_data["SecurityGroups"]
 
     instance_data = []
-    data = instances_data["Reservations"]
 
-    for entry in data:
-        instances = entry["Instances"] #- gives list
-        for instance in instances:
+    for instance in instances:
 
-            instance_id = instance["DB_id"]
-            instance_class = instance["DB_class"]
-            instance_status = instance["Status"]
-            instance_engine = instance["Engine"]
+        instance_id = instance["DBInstanceIdentifier"]
+        instance_sgs = []   
+        for sg_data in instance["VpcSecurityGroups"]:
+            instance_sgs.append(sg_data["VpcSecurityGroupId"])
 
-            instance_endpoint = []
-
-            instance_inbound = []
-            instance_outbound = []
-            instance_subnets = []
-
-            endpoint_data = instance["Endpoint"]
-            endpoint_simplified = {"Address": endpoint_data["Address"], "Port":endpoint_data["Port"]}
+        #print(instance_sgs)
+        instance_inbound = []
+        instance_outbound = []
+        instance_subnets = []
 
 
-            for inbound_data in database_data:
-                            
-                protocol = inbound_data["IpProtocol"]
-                from_port = "-1"
-                to_port = "-1"
-                if protocol != "-1":
-                    from_port = inbound_data["FromPort"]
-                    to_port = inbound_data["ToPort"]
-
-                if len(inbound_data["IpRanges"]) > 0:
-                    for ip in inbound_data["IpRanges"]:
-                        new_inbound_data_entry = {"Source": ip["CidrIp"], "Port":to_port, "Protocol": protocol}
-                        if new_inbound_data_entry not in instance_inbound:
-                            instance_inbound.append(new_inbound_data_entry)
-
-                        if len(inbound_data["Ipv6Ranges"]) > 0:
-                            for ipv6 in inbound_data["Ipv6Ranges"]:
-                                new_inbound_data_entry = {"Source": ipv6["CidrIpv6"], "Port":to_port, "Protocol": protocol}
-                                if new_inbound_data_entry not in instance_inbound:
-                                    instance_inbound.append(new_inbound_data_entry)
-                        
-                        if len(inbound_data["UserIdGroupPairs"]) > 0:
-                            for security_group_id in inbound_data["UserIdGroupPairs"]:
-                                new_inbound_data_entry = {"Source": security_group_id["GroupId"], "Port":to_port ,"Protocol": protocol}
-                                if new_inbound_data_entry not in instance_inbound:
-                                    instance_inbound.append(new_inbound_data_entry)
-
-                            
-                    for outbound_data in database_data:
-                        
-                        protocol = outbound_data["IpProtocol"]
+        for sg in instance_sgs:
+            # sg_id = sg["GroupId"]
+            for security_group in sg_data_list:
+                if sg == security_group["GroupId"]:
+                    #print(json.dumps(security_group, indent=4))
+                    for inbound_permission in security_group["IpPermissions"]:
+                        protocol = inbound_permission["IpProtocol"]
                         from_port = "-1"
                         to_port = "-1"
                         if protocol != "-1":
-                            from_port = outbound_data["FromPort"]
-                            to_port = outbound_data["ToPort"]
+                            from_port = inbound_permission["FromPort"]
+                            to_port = inbound_permission["ToPort"]
 
-                        if len(outbound_data["IpRanges"]) > 0:
-                            for ip in outbound_data["IpRanges"]:
-                                new_outbound_data_entry = {"Destination": ip["CidrIp"], "Port":to_port, "Protocol": protocol}
-                                if new_outbound_data_entry not in instance_outbound:
-                                    instance_outbound.append(new_outbound_data_entry)
+                        if len(inbound_permission["IpRanges"]) > 0:
+                            for ip in inbound_permission["IpRanges"]:
+                                new_inbound_permission_entry = {"Source": ip["CidrIp"], "Protocol": protocol, "Port":to_port}
+                                if new_inbound_permission_entry not in instance_inbound:
+                                    instance_inbound.append(new_inbound_permission_entry)
 
-                        if len(outbound_data["Ipv6Ranges"]) > 0:
-                            for ipv6 in outbound_data["Ipv6Ranges"]:
-                                new_outbound_data_entry = {"Destination": ipv6["CidrIpv6"], "Port":to_port, "Protocol": protocol}
-                                if new_outbound_data_entry not in instance_outbound:
-                                    instance_outbound.append(new_outbound_data_entry)
+                        if len(inbound_permission["Ipv6Ranges"]) > 0:
+                            for ipv6 in inbound_permission["Ipv6Ranges"]:
+                                new_inbound_permission_entry = {"Source": ipv6["CidrIpv6"], "Protocol": protocol, "Port":to_port}
+                                if new_inbound_permission_entry not in instance_inbound:
+                                    instance_inbound.append(new_inbound_permission_entry)
                         
-                        if len(outbound_data["UserIdGroupPairs"]) > 0:
-                            for security_group in outbound_data["UserIdGroupPairs"]:
-                                new_outbound_data_entry = {"Destination": security_group["GroupId"], "Port":to_port, "Protocol": protocol}
-                                if new_outbound_data_entry not in instance_outbound:
-                                    instance_outbound.append(new_outbound_data_entry)
+                        if len(inbound_permission["UserIdGroupPairs"]) > 0:
+                            for security_group_id in inbound_permission["UserIdGroupPairs"]:
+                                new_inbound_permission_entry = {"Source": security_group_id["GroupId"], "Protocol": protocol, "Port":to_port}
+                                if new_inbound_permission_entry not in instance_inbound:
+                                    instance_inbound.append(new_inbound_permission_entry)
                         
-                        if len(outbound_data["PrefixListIds"]) > 0:
-                            for prefixid in outbound_data["PrefixListIds"]:
-                                new_outbound_data_entry = {"Destination": prefixid, "Port":to_port, "Protocol": protocol}
-                                if new_outbound_data_entry not in instance_outbound:
-                                    instance_outbound.append(new_outbound_data_entry)
+                        if len(inbound_permission["PrefixListIds"]) > 0:
+                            for prefixid in inbound_permission["PrefixListIds"]:
+                                new_inbound_permission_entry = {"Source": prefixid, "Protocol": protocol, "Port":to_port}
+                                if new_inbound_permission_entry not in instance_inbound:
+                                    instance_inbound.append(new_inbound_permission_entry)
+                        
+                    for outbound_permission in security_group["IpPermissionsEgress"]:
+                        # print(outbound_permission)
+                        protocol = outbound_permission["IpProtocol"]
+                        from_port = "-1"
+                        to_port = "-1"
+                        if protocol != "-1":
+                            from_port = outbound_permission["FromPort"]
+                            to_port = outbound_permission["ToPort"]
 
-            subnet_data = instance["DBSubnetGroup"]["Subnets"]
-            for subnet in subnet_data:
-            	new_subnet_data_entry = {"Subnet_id":subnet["SubnetIdentifier"], "SubnetAvailabilityZone":subnet["SubnetAvailabilityZone"]["Name"], "Status":subnet["SubnetStatus"]}
-                if new_subnet_data_entry not in subnets_list:
-                    instance_subnets.append(new_subnet_data_entry)
-            
+                        if len(outbound_permission["IpRanges"]) > 0:
+                            for ip in outbound_permission["IpRanges"]:
+                                new_outbound_permission_entry = {"Source": ip["CidrIp"], "Protocol": protocol, "Port":to_port}
+                                if new_outbound_permission_entry not in instance_outbound:
+                                    instance_outbound.append(new_outbound_permission_entry)
 
-        instance_simplified = {"DB_id":instance_id, "DB_class":instance_class,"Status":instance_status, "Engine":instance_engine, "Outbound":instance_outbound, "Inbound":instance_inbound, "Subnets": subnets_list, "Endpoint": endpoint_simplified}
+                        if len(outbound_permission["Ipv6Ranges"]) > 0:
+                            for ipv6 in outbound_permission["Ipv6Ranges"]:
+                                new_outbound_permission_entry = {"Source": ipv6["CidrIpv6"], "Protocol": protocol, "Port":to_port}
+                                if new_outbound_permission_entry not in instance_outbound:
+                                    instance_outbound.append(new_outbound_permission_entry)
+                        
+                        if len(outbound_permission["UserIdGroupPairs"]) > 0:
+                            for security_group in outbound_permission["UserIdGroupPairs"]:
+                                new_outbound_permission_entry = {"Source": security_group["GroupId"], "Protocol": protocol, "Port":to_port}
+                                if new_outbound_permission_entry not in instance_outbound:
+                                    instance_outbound.append(new_outbound_permission_entry)
+                        
+                        if len(outbound_permission["PrefixListIds"]) > 0:
+                            for prefixid in outbound_permission["PrefixListIds"]:
+                                new_outbound_permission_entry = {"Source": prefixid, "Protocol": protocol, "Port":to_port}
+                                if new_outbound_permission_entry not in instance_outbound:
+                                    instance_outbound.append(new_outbound_permission_entry)
+        
+    
+        
+
+        instance_simplified = {"DB_id":instance_id, "SecurityGroups":instance_sgs,"Outbound":instance_outbound, "Inbound":instance_inbound}
         instance_data.append(instance_simplified)
         
 
