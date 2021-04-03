063e115a1ce58861915a5461ab21f3a0c320e1e0
Aaron Readman <readmana@tcd.ie>
2021-04-03 18:34:47 +0100

    graphscript now allows for multiple security groups aswell and singleNode allows for incoming connections

--- src/userinterfacesrc/src/Bootstrap/graphscript.js
+++ src/userinterfacesrc/src/Bootstrap/graphscript.js
@@ -9,16 +9,37 @@ var links = new Array();
 var categories = new Array();
 var testData = { nodes, links, categories };
 
+
+
+var securityGroupsArray = ["",""];
+
+
 myChart.showLoading();
 
 // generateSecurityGroup("");
 generateEntireGraph();
-// generateSingleNode("");//"");
+// generateSingleNodeOutgoing("");
+//  generateSingleNodeIncoming("");
+// generateSingleNode();
+//generateMultipleSecurityGroups(securityGroupsArray);
+
+/*
+Known Issues:
+
+The value of edges is showing the incorrect ports on occasions and it only shows a single info.source when there should be multiple
+Some of the font cases will be lower case. This isn't an actual issue  but it does make it more difficult to read the code
+
+*/
+
+
+
+
+
+
 
 /*
 General Purpose functions that are used in each of the graph generating functions
 */
-
 
 /*
 This function returns true/false dependind on whether there is an overlapping of links and
@@ -44,7 +65,7 @@ function nodeOverLapCheckerAndConcatenation(data,testData,k)
 
             testData.links[m].ports=newPorts;
 
-              var labelShown="----Source:---"+ data.links[k].info.source.toString()+"----Protocol:------"+data.links[k].info.protocol.toString()+"--ports:  "+newPorts;
+              var labelShown="----Source:   "+ data.links[k].info.source.toString()+"----Protocol:------"+data.links[k].info.protocol.toString()+"--ports:  "+newPorts;
               
               var value="value"
 
@@ -93,7 +114,7 @@ function createlinks(data,testData,oldLink,k)
               source: data.links[k].source,
               target: data.links[k].target,
               ports: portString ,
-              value: "----Source:---"+ data.links[k].info.source.toString()+"----Protocol:------"+data.links[k].info.protocol.toString()+"--ports:  "+portString,
+              value: "----Source:           "+ data.links[k].info.source.toString()+"----Protocol:------"+data.links[k].info.protocol.toString()+"Ports:            "+portString,
               lineStyle: {
                 color: "green",
                 curveness: 0.3
@@ -181,12 +202,17 @@ function determineNameAndType(data,i)
 
 
 
+
+
 /*
 Graph Generating Functions are below
 Current functions that have been implemented are:
 generateEntireGraph()
 generateSingleNode(nodeInput)
-generateSingleSecurityGroup(securityGroup Input)
+generateSingleNodeIncoming(nodeInput)
+generateSingleNodeOutgoing(nodeInput)
+generateSecurityGroup(securityGroup Input String)
+generateMultipleSecurityGroups(securityGroup Input Array)
 */
 
 
@@ -256,8 +282,104 @@ It then adds it to the testData.links array depending on whether or not the link
 
 
 
+function generateSingleNode(NodeID){
+  fetch("work.json")
+  .then((response) => response.json())
+  .then((data) => {
+    var i;
+    var numberOfnodes = 0;
 
-function generateSingleNode(NodeID) {
+    /*
+   This for loop iterates over the  links and identifies any node connected with the source Node.
+    It then adds it to an array of connectednodes
+   */
+
+    var connectednodes = new Array();
+    connectednodes.push(NodeID); //Source Node
+
+    
+    var k;
+    for (k = 0; k < data.links.length; k++) {
+        if (data.links[k].target == NodeID || data.links[k].source == NodeID) 
+        {
+          if (data.links[k].target == NodeID)
+          {
+            connectednodes.push(data.links[k].source);
+          }
+          else{
+            connectednodes.push(data.links[k].target);
+          }
+            
+        //Calling the concatenation function for ports and returning false if the current data.links[k] has no overlap in the testData.links
+        var oldLink=nodeOverLapCheckerAndConcatenation(data,testData,k);
+        createlinks(data,testData,oldLink,k);
+    }
+    }
+    console.log(testData.links);
+
+    /*
+This for loop iterates over the nodes array in the JSON data and converts it into a format
+that echarts will accept. It will only convert the desired nodes
+*/
+
+    for (i = 0; i < data.nodes.length; i++) {
+
+
+      var idOfNode;
+      var nameOfNode;
+      var nameAndTypeArray= new Array();
+      nameAndTypeArray =  determineNameAndType(data,i);
+      idOfNode=nameAndTypeArray[0];
+      nameOfNode=nameAndTypeArray[1];
+      
+
+      if ( connectednodes.includes(idOfNode)) 
+      {
+
+
+        /*
+              Creating a new array of nodes using the input JSON file
+              Includes a short if statement that checks if is the source Node
+        */
+        var sourceNode;
+        if (idOfNode == NodeID) {
+          sourceNode = true;
+        } else {
+          sourceNode = false;
+        }
+
+        testData.nodes[numberOfnodes] = {
+          id: idOfNode,
+          name: nameOfNode,
+          symbolSize: sourceNode ? 10 : 4,
+          value: 0,
+          category: data.nodes[i].type,
+          // symbol: "square",
+          itemStyle: {
+            color: sourceNode ? "blue" : "red",
+          },
+        };
+        numberOfnodes++;
+      
+    }
+  }
+
+    
+    createCategories(testData)
+
+    loadChart(testData);
+  });
+
+
+
+
+}
+
+
+
+
+
+function generateSingleNodeOutgoing(NodeID) {
   fetch("work.json")
     .then((response) => response.json())
     .then((data) => {
@@ -340,6 +462,102 @@ that echarts will accept. It will only convert the desired nodes
     });
 }
 
+function generateSingleNodeIncoming(NodeID){
+  fetch("work.json")
+  .then((response) => response.json())
+  .then((data) => {
+    var i;
+    var numberOfnodes = 0;
+
+    /*
+   This for loop iterates over the  links and identifies any node connected with the source Node.
+    It then adds it to an array of connectednodes
+   */
+
+    var connectednodes = new Array();
+    connectednodes.push(NodeID); //Source Node
+
+    
+    var k;
+    for (k = 0; k < data.links.length; k++) {
+      if (data.links[k].target == NodeID ) 
+      {
+        connectednodes.push(data.links[k].source);
+                   
+        //Calling the concatenation function for ports and returning false if the current data.links[k] has no overlap in the testData.links
+        var oldLink=nodeOverLapCheckerAndConcatenation(data,testData,k);
+        createlinks(data,testData,oldLink,k);
+    }
+    }
+    console.log(testData.links);
+
+    /*
+This for loop iterates over the nodes array in the JSON data and converts it into a format
+that echarts will accept. It will only convert the desired nodes
+*/
+
+    for (i = 0; i < data.nodes.length; i++) {
+
+
+      var idOfNode;
+      var nameOfNode;
+      var nameAndTypeArray= new Array();
+      nameAndTypeArray =  determineNameAndType(data,i);
+      idOfNode=nameAndTypeArray[0];
+      nameOfNode=nameAndTypeArray[1];
+      
+
+      if ( connectednodes.includes(idOfNode)) 
+      {
+
+
+        /*
+              Creating a new array of nodes using the input JSON file
+              Includes a short if statement that checks if is the source Node
+        */
+        var sourceNode;
+        if (idOfNode == NodeID) {
+          sourceNode = true;
+        } else {
+          sourceNode = false;
+        }
+
+        testData.nodes[numberOfnodes] = {
+          id: idOfNode,
+          name: nameOfNode,
+          symbolSize: sourceNode ? 10 : 4,
+          value: 0,
+          category: data.nodes[i].type,
+          // symbol: "square",
+          itemStyle: {
+            color: sourceNode ? "blue" : "red",
+          },
+        };
+        numberOfnodes++;
+      
+    }
+  }
+
+    
+    createCategories(testData)
+
+    loadChart(testData);
+  });
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
 function generateSecurityGroup(securityGroups) {
   fetch("work.json")
     .then((response) => response.json())
@@ -392,6 +610,90 @@ function generateSecurityGroup(securityGroups) {
       loadChart(testData);
     });
 }
+
+
+
+
+function generateMultipleSecurityGroups(securityGroups) {
+  fetch("work.json")
+    .then((response) => response.json())
+    .then((data) => {
+      var i;
+      var numberOfnodes = 0;
+
+      var k;
+      for (k = 0; k < data.links.length; k++) 
+      {
+        var oldLink=nodeOverLapCheckerAndConcatenation(data,testData,k);
+        createlinks(data,testData,oldLink,k);
+      }
+
+      //Create Nodes in testData.nodes
+      for (i = 0; i < data.nodes.length; i++) {
+
+
+        //Short for loop to determine if the node contains the some of the required securityGroups
+        var containsSecurityGroup=false;
+        var t;
+        var sg;
+        for(t=0;t<securityGroups.length;t++)
+        {
+          sg=securityGroups[t];
+         if( data.nodes[i].securityGroups.includes(sg))
+         {
+          containsSecurityGroup=true;
+         }
+        }
+
+
+        if ( containsSecurityGroup==true) {
+
+          var idOfNode;
+          var nameOfNode;
+          var nameAndTypeArray= new Array();
+          nameAndTypeArray =  determineNameAndType(data,i);
+          idOfNode=nameAndTypeArray[0];
+          nameOfNode=nameAndTypeArray[1];
+
+          //Creating the node
+          testData.nodes[numberOfnodes] = {
+            id: idOfNode,
+            name: nameOfNode,
+            symbolSize: 3,
+            value: 0,
+            category: data.nodes[i].type,
+            symbol: "square",
+            
+          };
+          numberOfnodes++;
+        }
+      }
+      console.log(testData);
+
+      //Create categories
+      createCategories(testData)
+
+      loadChart(testData);
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function loadChart(graph) {
   myChart.hideLoading();
